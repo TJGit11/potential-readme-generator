da2ea73784d4756adedba95a4f10162f9a525e79
unknown <thomasjjohnson9311@gmail.com>
2023-04-26 23:11:49 -0500

    it works!

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# potential-readme-generator
+
+
+# Title
+Bread
+
+## Project Description
+Breaded cheese
+
+## License
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 
 // let response = await inquirer.prompt([
 // structured formate
-let { description, last_name } = await inquirer.prompt([
+let { project_title, project_description, license } = await inquirer.prompt([
   // destructured format
   {
     type: "input",
@@ -26,29 +26,29 @@ let { description, last_name } = await inquirer.prompt([
   },
 ]);
 
-console.log(first_name, last_name);
+console.log({ project_title, project_description, license });
 
-let readmeText = `# Project Description
-${description}
+let readmeText = `
 
-## The second largest heading
+# Title
+${project_title}
+
+## Project Description
+${project_description}
+
+## License
 ${generateLicense(license)}
-
-### The third largest heading
-
-
-##### The smallest heading 
 
 `;
 
 fs.writeFile("README.md", readmeText);
 
 function generateLicense(license) {
-  if (license === "Mozilla") {
+  if (license === "mozilla") {
     return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-  } else if (license === "Artistic") {
+  } else if (license === "artistic") {
     return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
-  } else if (license === "Eclipse") {
+  } else if (license === "eclipse") {
     return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-  } else return "No license selected";
+  } else return "n";
 }
