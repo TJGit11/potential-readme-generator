f79cabb420edd57e73f9294bc753f9787fa16688
unknown <thomasjjohnson9311@gmail.com>
2023-04-26 22:15:32 -0500

    licenses selected

--- index.mjs
+++ index.mjs
@@ -19,7 +19,7 @@ let { description, last_name } = await inquirer.prompt([
     type: "list",
     name: "license",
     message: "Which license would you like to use?",
-    choices: ["Mozilla, "],
+    choices: ["Mozilla", "Artistic", "Eclipse"],
     filter(val) {
       return val.toLowerCase();
     },
@@ -32,7 +32,7 @@ let readmeText = `# Project Description
 ${description}
 
 ## The second largest heading
-${generateLicense(size)}
+${generateLicense(license)}
 
 ### The third largest heading
 
@@ -46,9 +46,9 @@ fs.writeFile("README.md", readmeText);
 function generateLicense(license) {
   if (license === "Mozilla") {
     return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-  } else if (license === "") {
-    return;
-  } else if (license === "") {
-    return;
+  } else if (license === "Artistic") {
+    return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+  } else if (license === "Eclipse") {
+    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
   } else return "No license selected";
 }
