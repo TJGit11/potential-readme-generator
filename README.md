46c1642c7ad0493a612af15d45fe17517ecb1d89
unknown <thomasjjohnson9311@gmail.com>
2023-05-01 00:00:12 -0500

    v4

--- README.md
+++ README.md
@@ -1,20 +1,40 @@
 # README Generator Project
+
 README Generator Project
 
 ## Table of Contents
+
 - [Project Description](#project-description)
+- [Installation](#installation)
 - [Usage](#usage)
-- [Contact](#contact)
 - [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Project Description
-This creates a README for you
+
+This project generates a README for you
+
+## Installation
+
+n/a
 
 ## Usage
 
-
-## Contact
-https://github.com/TJGit11/potential-readme-generator
-
 ## License
+
 [![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
+
+## Contributing
+
+n/a
+
+## Tests
+
+n/a
+
+## Questions
+
+If you have any questions, you can find my github profile here or you can send me an email
+https://github.com/TJGit11 thomasjjohnson9311@gmail.com
--- index.mjs
+++ index.mjs
@@ -3,62 +3,109 @@ import fs from "fs/promises";
 
 // let response = await inquirer.prompt([
 // structured formate
-let { project_title, project_description, usage, contact, license } =
-  await inquirer.prompt([
-    // destructured format
-    {
-      type: "input",
-      name: "project_title",
-      message: "What is the name of your project?",
+let {
+  project_title,
+  project_description,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  questions,
+} = await inquirer.prompt([
+  // destructured format
+  {
+    type: "input",
+    name: "project_title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "project_description",
+    message: "Describe your project here",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Please enter the necessary installation steps for use of this project",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Helpful screenshots go here",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: ["Mozilla", "Artistic", "Eclipse"],
+    filter(val) {
+      return val.toLowerCase();
     },
-    {
-      type: "input",
-      name: "project_description",
-      message: "Describe your project here",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Helpful screenshots go here",
-    },
-    {
-      type: "input",
-      name: "contact",
-      message: "Helpful links go here",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "Which license would you like to use?",
-      choices: ["Mozilla", "Artistic", "Eclipse"],
-      filter(val) {
-        return val.toLowerCase();
-      },
-    },
-  ]);
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Contributions go here",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Testing results go here",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Please enter your github profile and email address here",
+  },
+]);
 
-console.log({ project_title, project_description, usage, contact, license });
+console.log({
+  project_title,
+  project_description,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  questions,
+});
 
 let readmeText = `# ${project_title}
 ${project_title}
 
 ## Table of Contents
 - [Project Description](#project-description)
+- [Installation](#installation)
 - [Usage](#usage)
-- [Contact](#contact)
 - [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Project Description
 ${project_description}
 
+## Installation
+${installation}
+
 ## Usage
 ${usage}
 
-## Contact
-${contact}
-
 ## License
 ${generateLicense(license)}
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions, you can find my github profile here or you can send me an email ${questions}
+
+
 `;
 
 fs.writeFile("README.md", readmeText);
